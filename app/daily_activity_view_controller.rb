bd9f099edc42897942770535081d8d3b9f2267a0
Hursh Agrawal <h@hurshagrawal.com>
2013-12-28 22:45:43 +0530

    added custom cell classes

--- app/daily_activity_view_controller.rb
+++ app/daily_activity_view_controller.rb
@@ -8,9 +8,9 @@ class DailyActivityViewController < UIViewController
     self.view = UITableView.alloc.initWithFrame(App.frame, style:UITableViewStyleGrouped)
     self.view.delegate = self
     self.view.dataSource = self
+
     self.view.reloadData
   end
-
 
   ## UITableViewDataSource
   DAILY_ACTIVITY_SECTION_INDEX = 0
@@ -29,17 +29,78 @@ class DailyActivityViewController < UIViewController
   end
 
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    identifier = case indexPath.section
-       when DAILY_ACTIVITY_SECTION_INDEX
-         :daily_activity
-       when NEW_ACTIVITY_SECTION_INDEX
-         :new_activity
-     end
+    if indexPath.section == DAILY_ACTIVITY_SECTION_INDEX
+      cell = cellForClass(DailyActivityTableViewCell, identifier:"daily_activity")
 
-    if cell = tableView.dequeueReusableCellWithIdentifier(identifier)
+      # Add relevant data
+    elsif indexPath.section == NEW_ACTIVITY_SECTION_INDEX
+      if indexPath.row == tableView(self.view, numberOfRowsInSection:indexPath.section) - 1 # If last row
+        cell = cellForClass(NewActivityFormTableViewCell, identifier:"new_activity_form")
+
+      else
+        cell = cellForClass(NewActivityTableViewCell, identifier:"new_activity")
+
+        # Add relevant data
+      end
+    end
+
+    cell
+  end
+
+  def cellForClass(cellClass, identifier:identifier)
+    if cell = self.view.dequeueReusableCellWithIdentifier(identifier)
       cell
     else
-      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:identifier)
+      cellClass.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:identifier)
+    end
+  end
+
+  ## Templating
+  class NewActivityFormTableViewCell < UITableViewCell
+    include Teacup::Layout
+
+    def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+      super.tap do
+        layout self.contentView do
+          subview UILabel,
+            text: "New Activity form yo",
+            top: 0, left: 0,
+            width: 200, height: 30,
+            textColor: UIColor.greenColor
+        end
+      end
+    end
+  end
+
+  class NewActivityTableViewCell < UITableViewCell
+    include Teacup::Layout
+
+    def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+      super.tap do
+        layout self.contentView do
+          subview UILabel,
+            text: "this is new activity",
+            top: 0, left: 0,
+            width: 200, height: 30,
+            backgroundColor: UIColor.blueColor
+        end
+      end
+    end
+  end
+
+  class DailyActivityTableViewCell < UITableViewCell
+    include Teacup::Layout
+
+    def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+      super.tap do
+        layout self.contentView do
+          subview UILabel,
+            text: "Sup label here",
+            top: 0, left: 0,
+            width: 200, height: 30,
+            backgroundColor: UIColor.blackColor
+        end
+      end
     end
   end
 end
